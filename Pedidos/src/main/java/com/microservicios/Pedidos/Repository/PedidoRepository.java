c2f6065fecffc9500460e6b2a18f136dd0025bcf
nicoRepos <119765859+noarphelp@users.noreply.github.com>
2025-02-20 12:24:37 +0100

    Delete Pedidos directory

--- Pedidos/src/main/java/com/microservicios/Pedidos/Repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservicios.Pedidos.Repository;
-
-import com.microservicios.Pedidos.Model.Pedidos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedidos ,Long> {
-}
